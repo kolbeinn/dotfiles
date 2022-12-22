560303b9141eb8f112ca2f18eab95d5f92d5ab05
Kolbeinn Karlsson <kolbeinn.karlsson@lacework.net>
2022-12-22 07:07:43 +0000

    Use apt-get instead of apt
    Signed-off-by: Kolbeinn Karlsson <kolbeinn.karlsson@lacework.net>

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 BREW=/home/linuxbrew/.linuxbrew/bin/brew
+APT=/usr/bin/apt-get
 
 $BREW install rg
 $BREW install lacework/tap/lacework-cli
 $BREW install neovim
 $BREW install fzf
+
+sudo $APT update
+sudo $APT install net-tools
